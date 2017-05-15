3389c1509db9d1f104c3d5320db04d0a39e0ad1b
Christopher Brown <io@henrian.com>
2017-05-15 01:03:24 -0500

    Add test profile; define source-paths

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :description "JSON parser/generator to/from Clojure data structures"
   :url "https://github.com/chbrown/data.json"
   :license {:name "Eclipse Public License"
-            :url "http://opensource.org/licenses/eclipse-1.0.php"})
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
+  :source-paths ["src/main/clojure"]
+  :profiles {:test {:source-paths ["src/main/clojure" "src/test/clojure"]
+                    :dependencies [[org.clojure/clojure "1.3.0"]]}})
