359c76ad2cd6314fe29a1b07e74e0ee225938b0e
Christopher Brown <io@henrian.com>
2017-05-15 00:07:42 -0500

    Add minimal Leiningen config

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject chbrown/data.json "0.2.6"
+  :description "JSON parser/generator to/from Clojure data structures."
+  :url "https://github.com/chbrown/data.json"
+  :license {:name "Eclipse Public License"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"})
