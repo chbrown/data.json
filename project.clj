52feae62783696bfafe333a3eb0145761126ee0e
Christopher Brown <io@henrian.com>
2017-05-15 00:35:28 -0500

    Set version to next patch-level with -SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject chbrown/data.json "0.2.6"
-  :description "JSON parser/generator to/from Clojure data structures."
+(defproject chbrown/data.json "0.2.7-SNAPSHOT"
+  :description "JSON parser/generator to/from Clojure data structures"
   :url "https://github.com/chbrown/data.json"
   :license {:name "Eclipse Public License"
             :url "http://opensource.org/licenses/eclipse-1.0.php"})
